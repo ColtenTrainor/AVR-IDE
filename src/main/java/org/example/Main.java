58b9456ecb74d0737bf2f18985e108fa13ac2218
ColtenTrainor <90798178+ColtenTrainor@users.noreply.github.com>
2023-05-30 21:06:50 -0400

    replaced gavrasm with avra, windows binary is very bbroken

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,17 @@ package org.example;
 import java.io.File;
 
 public class Main {
+
+    public static final String OperatingSystem = System.getProperty("os.name");
+
     public static void main(String[] args) {
-        System.out.println("---" + System.getProperty("os.name"));
+        System.out.println("---" + OperatingSystem);
         File currentDirectory = new File("");
         System.out.println("Abs path: " + currentDirectory.getAbsolutePath());
 
         // Windows
         CommandExecutor commandExecutor = new CommandExecutor();
-        commandExecutor.runCommand(new File(currentDirectory + "gavrasm"), "gavrasm.exe", "instr.asm");
+        commandExecutor.runCommand(new File(currentDirectory + "avra"), "avra.exe", "test.asm");
 //        commandExecutor.runCommand("dir");
 //        commandExecutor.runCommand("ipconfig", "/all");
 
