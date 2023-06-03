a187f5b0bb7c36170a7c26786b5ca1fd5ba208e3
ColtenTrainor <90798178+ColtenTrainor@users.noreply.github.com>
2023-06-03 15:22:16 -0400

    implemented settings config file and save data path (de)serialization

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,14 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public enum OS { Windows, Linux, Mac }
@@ -13,20 +21,33 @@ public class Main {
         else throw new RuntimeException("Failed to identify operating system");
     };
     public static File SaveDataDir;
+    static {
+        var cfg = new File("settings.cfg");
+        Gson gson = new Gson();
+        try {
+            if (cfg.createNewFile()) {
+                var writer = new FileWriter(cfg);
+                var savepath = FileSystemView.getFileSystemView()
+                        .getDefaultDirectory().getAbsolutePath();
+                if (OperatingSystem == OS.Linux) savepath += "/Documents";
+                savepath += "/AVR-IDE";
+                SaveDataDir = new File(savepath);
+                writer.write(gson.toJson(SaveDataDir.getAbsolutePath()));
+                writer.close();
+            } else {
+                var scanner = new Scanner(cfg);
+                SaveDataDir = new File(gson.fromJson(scanner.nextLine(), String.class));
+                scanner.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to read or write settings.cfg");
+            throw new RuntimeException(e);
+        }
+    }
     public static void main(String[] args) {
+        System.out.println(SaveDataDir);
+        System.out.println(SaveDataDir.mkdir());
 
-
-        System.out.println("---" + OperatingSystem);
-        File currentDirectory = new File("");
-        System.out.println("Abs path: " + currentDirectory.getAbsolutePath());
-
-        // Windows
-        CommandExecutor commandExecutor = new CommandExecutor();
-//        commandExecutor.runCommand(new File(currentDirectory + "avra"), "avra.exe", "test.asm");
-//        commandExecutor.runCommand("dir");
-//        commandExecutor.runCommand("ipconfig", "/all");
-
-        // Linux
-//        commandExecutor.runCommand("pwd");
+        var commandExecutor = new CommandExecutor();
     }
 }
