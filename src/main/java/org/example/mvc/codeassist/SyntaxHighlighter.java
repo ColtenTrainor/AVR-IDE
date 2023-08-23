292a04ec0a1102a5057169b357e14c0584895b35
ColtenTrainor <90798178+ColtenTrainor@users.noreply.github.com>
2023-08-23 02:19:04 -0400

    refactored syntax highlighter to be more robust for editing the regex

--- src/main/java/org/example/mvc/codeassist/SyntaxHighlighter.java
+++ src/main/java/org/example/mvc/codeassist/SyntaxHighlighter.java
@@ -21,13 +21,20 @@ public class SyntaxHighlighter implements Runnable{
     private Pattern highlightPattern;
     private String[] categories;
 
+    private static final HashMap<String, String> initialCategories = new HashMap<>();
+    static {
+        initialCategories.put("ignore", "(;[^\\n]*)");
+        initialCategories.put("constant", "(\\b(?:(?:0b[0-1]*)|(?:0x(?:\\d|[a-f])*)|\\d+)\\b)");
+        initialCategories.put("directive", "\\s(\\.\\S+)\\s");
+        initialCategories.put("register", "(\\b(?:(?:r\\d+)|(?:(?:X|Y|Z)(?:H|L)))\\b)");
+    }
+
     public SyntaxHighlighter(StyledDocument document, JTextPane textPane) {
         this.document = document;
         this.textPane = textPane;
         this.defaultStyle = textPane.addStyle("default-style", null);
         this.highlightStyle = textPane.addStyle("highlight-style", defaultStyle);
-        this.categories = new String[4 + InstructionData.getCategorySet().size()];
-        initCategoryArray();
+        this.categories = new String[initialCategories.size() + InstructionData.getCategorySet().size()];
 
         ConfigIOHandler configHandler = new ConfigIOHandler(
                 new File("cfg/highlightColors.cfg"));
@@ -35,17 +42,6 @@ public class SyntaxHighlighter implements Runnable{
         decodeColorData(configHandler.getConfigData());
 
         compileHighlightPattern();
-    }
-
-    private void initCategoryArray(){
-        this.categories[0] = "ignore";
-        this.categories[1] = "constant";
-        this.categories[2] = "directive";
-        this.categories[3] = "register";
-        var categoryIterator = InstructionData.getCategorySet().iterator();
-        for (int i = 4; i < categories.length; i++) {
-            categories[i] = categoryIterator.next();
-        }
     }
 
     private void decodeColorData(HashMap<String, String> data) {
@@ -61,13 +57,20 @@ public class SyntaxHighlighter implements Runnable{
 
     private void compileHighlightPattern() {
         var pattern = new StringBuilder();
-        pattern.append("(;[^\\n]*)") // comment
-                .append("|(\\b(?:(?:0b[0-1]*)|(?:0x(?:\\d|[a-f])*)|\\d+)\\b)") // constant
-                .append("|\\s(\\.\\S+)\\s") // directive
-                .append("|(\\b(?:(?:r\\d+)|(?:(?:X|Y|Z)(?:H|L)))\\b)"); // register
 
-        for (int i = 4; i < categories.length; i++) {
-            appendWordList(pattern, categories[i]);
+        int categoryIndex = 0;
+
+        for (String s : initialCategories.keySet()){
+            pattern.append(initialCategories.get(s)).append("|");
+            categories[categoryIndex] = s;
+            categoryIndex++;
+        }
+
+        for (String s : InstructionData.getCategorySet()) {
+            appendInstructionRegex(pattern, s);
+            if (categoryIndex < categories.length - 1) pattern.append("|");
+            categories[categoryIndex] = s;
+            categoryIndex++;
         }
 
         System.out.println(pattern);
@@ -76,8 +79,8 @@ public class SyntaxHighlighter implements Runnable{
         highlightPattern = Pattern.compile(pattern.toString(), Pattern.CASE_INSENSITIVE);
     }
 
-    private void appendWordList(StringBuilder pattern, String category) {
-        pattern.append("|(\\b(?:");
+    private void appendInstructionRegex(StringBuilder pattern, String category) {
+        pattern.append("(\\b(?:");
         var instructionSet = InstructionData.getInstructionSetFromCategory(category).toArray();
         for (int i = 0; i < instructionSet.length - 1; i++) {
             pattern.append(instructionSet[i]).append("|");
